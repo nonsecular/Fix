ba10e410ef659904836b932bbe33ebc891ef8062
nonsecular <nonsecularkrish@gmail.com>
2026-02-22 17:51:25 +0530

    Update support channel and chat URLs

--- config.py
+++ config.py
@@ -56,8 +56,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NexaCoders")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NexaMeetup")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/iscamz")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/kryshupdate")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", None))
