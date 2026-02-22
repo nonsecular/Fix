6d8adbe5bb53221c234ee91830b788fa0362fc8d
nonsecular <nonsecularkrish@gmail.com>
2026-02-22 13:40:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/nonsecular/Fix/blob/master/config.py",
+    "https://github.com/nonsecular/Fix",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
